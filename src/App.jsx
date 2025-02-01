bfeada5d600dcdcbae5e4d103041b4741bc2bebd
Damjan15 <stamenovdamjan5@gmail.com>
2025-02-01 09:33:31 +0100

    feat: MovieCard component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
 
 // API Settings
 const API_BASE_URL = "https://api.themoviedb.org/3";
@@ -78,9 +79,7 @@ function App() {
           ) : (
             <ul>
               {movieList.map((movie) => (
-                <p className="text-white" key={movie.id}>
-                  {movie.title}
-                </p>
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
